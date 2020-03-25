0e43287facce37e58eda201e3a9acfbb9de6d471
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-03-25 20:37:05 +0100

    Add model types

---
+++ testbeds/kubernetes/cassandra-monitoring/twitter-clone/src/model/tweet.ts
@@ -0,0 +1,13 @@
+
+export interface GeoLocation {
+    latitude: number;
+    longitude: number;
+}
+
+export interface Tweet {
+    tweet_id: string;
+    username: string;
+    tweet_body: string;
+    time: number;
+    location: GeoLocation;
+}
