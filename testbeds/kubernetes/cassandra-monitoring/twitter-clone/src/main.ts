39347a512deac085d96ae440b0e50ed33477082a
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-03-25 20:33:39 +0100

    Add Cassandra connection

--- testbeds/kubernetes/cassandra-monitoring/twitter-clone/src/main.ts
+++ testbeds/kubernetes/cassandra-monitoring/twitter-clone/src/main.ts
@@ -1,13 +1,54 @@
+import { Client } from 'cassandra-driver';
 import express from 'express';
+import { convertToNumber, getEnvironmentVariable } from './util/environment';
 
-const port = 8080;
+const config = {
+    port: getEnvironmentVariable('LISTEN_PORT', convertToNumber) || 8080,
+    cassandraNode: getEnvironmentVariable('CASSANDRA_NODE'),
+    cassandraPort: getEnvironmentVariable('CASSANDRA_PORT', convertToNumber) || 9042,
+    cassandraUser: getEnvironmentVariable('CASSANDRA_USER'),
+    cassandraPassword: getEnvironmentVariable('CASSANDRA_PWD'),
+};
 
-const app = express();
+if (!config.cassandraNode) {
+    throw new Error(
+        'No Cassandra node host specified. Please set the environment variable CASSANDRA_HOST to the host of the Cassandra node that this application should connect to.',
+    );
+}
 
-app.get('/', (req, res) => {
-    res.send('hallo!');
-});
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}.`);
-});
+function connectToDb(): Promise<Client> {
+    console.log('Connecting to Cassandra...');
+
+    const cassandraClient = new Client({
+        contactPoints: [ `${config.cassandraNode}:${config.cassandraPort}` ],
+        credentials: config.cassandraUser ? { username: config.cassandraUser, password: config.cassandraPassword } : undefined,
+        localDataCenter: 'dc1',
+    });
+
+    return cassandraClient.connect()
+        .then(() => {
+            console.log('Successfully connected to Cassandra DB.')
+            return cassandraClient
+        });
+}
+
+function initRestApi(cassandraClient: Client): void {
+    const app = express();
+
+    app.get('/', (req, res) => {
+        res.send('hallo!');
+    });
+
+    app.listen(config.port, () => {
+        console.log(`Listening on port ${config.port}.`);
+    });
+}
+
+async function init(): Promise<void> {
+    const cassandra = await connectToDb();
+    initRestApi(cassandra);
+}
+
+
+init();
