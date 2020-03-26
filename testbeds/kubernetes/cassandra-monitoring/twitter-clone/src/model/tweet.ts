68ee13c078b54b072b96053ccd6b65bf1b9c2325
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-03-26 16:42:59 +0100

    Implement TweetsController

--- testbeds/kubernetes/cassandra-monitoring/twitter-clone/src/model/tweet.ts
+++ testbeds/kubernetes/cassandra-monitoring/twitter-clone/src/model/tweet.ts
@@ -1,3 +1,4 @@
+import { types as CassandraTypes } from 'cassandra-driver';
 
 export interface GeoLocation {
     latitude: number;
@@ -5,7 +6,7 @@ export interface GeoLocation {
 }
 
 export interface Tweet {
-    tweet_id: string;
+    tweet_id: CassandraTypes.TimeUuid;
     username: string;
     tweet_body: string;
     time: Date;
--- testbeds/kubernetes/cassandra-monitoring/twitter-clone/src/resource-controllers/tweets/tweets.controller.ts
+++ testbeds/kubernetes/cassandra-monitoring/twitter-clone/src/resource-controllers/tweets/tweets.controller.ts
@@ -1,15 +1,71 @@
-import { ResourceControllerBase } from '../resource.controller.base';
 import { types as CassandraTypes } from 'cassandra-driver';
-import { Express } from 'express';
+import { Express, Request, Response } from 'express';
+import { Tweet } from '../../model';
+import { DbModelMapper, generateRandomLocation, ModelType } from '../../util';
+import { ResourceControllerBase } from '../resource.controller.base';
 
-const queries = {
+const TWEETS_PREFIX = '/tweets';
 
-};
+const TWEETS_COUNT = 1000;
 
 export class TweetsController extends ResourceControllerBase {
 
-    protected registerEndpointsInternal(express: Express): void {
+    private tweetsMapper: DbModelMapper<Tweet>;
 
+    protected registerEndpointsInternal(express: Express): void {
+        this.tweetsMapper = this.dbMapper.forModel(ModelType.Tweet);
+
+        express.get(
+            TWEETS_PREFIX,
+            (req, res) => this.executeSafely(req, res, () => this.getMostRecentTweets(req, res)),
+        );
+
+        express.get(
+            `${TWEETS_PREFIX}/user/:username`,
+            (req, res) => this.executeSafely(req, res, () => this.getMostRecentTweetsByUser(req, res)),
+        );
+
+        express.put(
+            `${TWEETS_PREFIX}/user/:username`,
+            (req, res) => this.executeSafely(req, res, () => this.createTweet(req, res)),
+        );
+
+        express.get(
+            `${TWEETS_PREFIX}/location/:lat/:long`,
+            (req, res) => this.executeSafely(req, res, () => this.getMostRecentTweetsByLocation(req, res)),
+        );
+    }
+
+    private async getMostRecentTweets(req: Request, res: Response): Promise<void> {
+        const result = await this.tweetsMapper.findAll({ limit: TWEETS_COUNT });
+        this.sendJsonResponse(res, result.toArray());
+    }
+
+    private async getMostRecentTweetsByUser(req: Request, res: Response): Promise<void> {
+        const result = await this.tweetsMapper.find({ username: req.params.username }, { limit: TWEETS_COUNT });
+        this.sendJsonResponse(res, result.toArray());
+    }
+
+    private async createTweet(req: Request, res: Response): Promise<void> {
+        const tweet: Tweet = req.body;
+        if (!tweet || !tweet.tweet_body) {
+            this.sendErrorResponse(res, 'You must specify a Tweet body.', { statusCode: 400 });
+            return;
+        }
+
+        tweet.username = req.params.username;
+        tweet.time = new Date();
+        tweet.tweet_id = CassandraTypes.TimeUuid.fromDate(tweet.time);
+        if (!tweet.location) {
+            tweet.location = generateRandomLocation();
+        }
+
+        await this.tweetsMapper.insert(tweet);
+        this.sendJsonResponse(res, tweet);
+    }
+
+    private async getMostRecentTweetsByLocation(req: Request, res: Response): Promise<void> {
+        throw new Error('This endpoint is not implemented.');
     }
 
 }
