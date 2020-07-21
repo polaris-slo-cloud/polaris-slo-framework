76fd96cf381531253f1b579246ebdf3b2ec2a1eb
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-07-21 17:35:45 +0200

    Move Grafana display to visualization module

--- ts/apps/sloc-ui/src/app/control-center/components/control-center/control-center.component.ts
+++ ts/apps/sloc-ui/src/app/control-center/components/control-center/control-center.component.ts
@@ -1,11 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of as observableOf } from 'rxjs';
-
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { distinctUntilChanged, map, switchMap } from 'rxjs/operators';
-import { DashboardDTO } from '../../../common/model';
-import { DashboardOperations } from '../../../core';
-import { PanelIdentifier } from '../../../shared';
 
 @Component({
     selector: 'sloc-control-center',
@@ -14,35 +7,9 @@ import { PanelIdentifier } from '../../../shared';
 })
 export class ControlCenterComponent implements OnInit {
 
-    panelForm: FormGroup;
-    selectedDashboard$: Observable<DashboardDTO>;
-
-    constructor(
-        private dashboardOps: DashboardOperations,
-    ) { }
+    constructor() { }
 
     ngOnInit(): void {
-        this.initForm();
-    }
-
-    private initForm(): void {
-        this.panelForm = new FormGroup({
-            dashboardUid: new FormControl(null, Validators.required),
-            panelId: new FormControl(null, Validators.required),
-        });
-
-        this.selectedDashboard$ = this.panelForm.valueChanges.pipe(
-            map((value: PanelIdentifier) => value.dashboardUid),
-            distinctUntilChanged(),
-            switchMap(dashboardUid => {
-                this.panelForm.patchValue({ panelId: null });
-                if (dashboardUid) {
-                    return this.dashboardOps.getDashboardByUid(dashboardUid);
-                } else {
-                    return observableOf(null);
-                }
-            }),
-        );
     }
 
 }
--- ts/apps/sloc-ui/src/app/visualization/components/visualization-master/visualization-master.component.ts
+++ ts/apps/sloc-ui/src/app/visualization/components/visualization-master/visualization-master.component.ts
@@ -1,4 +1,11 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable, of as observableOf } from 'rxjs';
+
+import { distinctUntilChanged, map, switchMap } from 'rxjs/operators';
+import { DashboardDTO } from '../../../common';
+import { DashboardOperations } from '../../../core';
+import { PanelIdentifier } from '../../../shared';
 
 @Component({
     selector: 'sloc-visualization-master',
@@ -8,9 +15,35 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 })
 export class VisualizationMasterComponent implements OnInit {
 
-    constructor() { }
+    panelForm: FormGroup;
+    selectedDashboard$: Observable<DashboardDTO>;
+
+    constructor(
+        private dashboardOps: DashboardOperations,
+    ) { }
 
     ngOnInit(): void {
+        this.initForm();
+    }
+
+    private initForm(): void {
+        this.panelForm = new FormGroup({
+            dashboardUid: new FormControl(null, Validators.required),
+            panelId: new FormControl(null, Validators.required),
+        });
+
+        this.selectedDashboard$ = this.panelForm.valueChanges.pipe(
+            map((value: PanelIdentifier) => value.dashboardUid),
+            distinctUntilChanged(),
+            switchMap(dashboardUid => {
+                this.panelForm.patchValue({ panelId: null });
+                if (dashboardUid) {
+                    return this.dashboardOps.getDashboardByUid(dashboardUid);
+                } else {
+                    return observableOf(null);
+                }
+            }),
+        );
     }
 
 }
