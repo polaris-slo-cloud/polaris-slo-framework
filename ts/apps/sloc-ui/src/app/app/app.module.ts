802e328896708ce17c78cd5a35730ca4932aae5c
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-07-21 15:38:06 +0200

    Create distinct folder for app module

--- ts/apps/sloc-ui/src/app/app/app.module.ts
+++ ts/apps/sloc-ui/src/app/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from '../app-routing.module';
+import { CoreModule } from '../core/core.module';
+import { SharedModule } from '../shared/shared.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
 
 const PROVIDERS: any[] = [
     { provide: LocationStrategy, useClass: HashLocationStrategy },
