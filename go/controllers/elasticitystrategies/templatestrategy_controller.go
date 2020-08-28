693a5b8fffa424a37c07b2f695834e25a476ece3
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-08-28 18:12:01 +0200

    Add fetching of TemplateStrategy and owned HPAs

--- go/controllers/elasticitystrategies/templatestrategy_controller.go
+++ go/controllers/elasticitystrategies/templatestrategy_controller.go
@@ -18,11 +18,13 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	autoscaling "k8s.io/api/autoscaling/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	elasticitystrategiesv1 "sloc.github.io/sloc/apis/elasticitystrategies/v1"
+	elasticityStrategies "sloc.github.io/sloc/apis/elasticitystrategies/v1"
+	"sloc.github.io/sloc/internal/util"
 )
 
 // TemplateStrategyReconciler reconciles a TemplateStrategy object
@@ -34,21 +36,47 @@ type TemplateStrategyReconciler struct {
 
 // +kubebuilder:rbac:groups=elasticitystrategies.sloc.github.io,resources=templatestrategies,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=elasticitystrategies.sloc.github.io,resources=templatestrategies/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=autoscaling,resources=hpa,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is called by the manager, whenever its underlying informers report that an
 // object has been added, updated, or deleted.
 func (r *TemplateStrategyReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("templatestrategy", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("templateStrategy", req.NamespacedName)
 
-	// your logic here
+	// Fetch the TemplateStrategy that we need to reconcile
+	var templateStrategy elasticityStrategies.TemplateStrategy
+	if err := r.Get(ctx, req.NamespacedName, &templateStrategy); err != nil {
+		log.Error(err, "Unable to fetch TemplateStrategy")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+	log.Info("Successfully fetched TemplateStrategy")
+
+	// Fetch the HPAs associated with this strategy.
+	var hpas autoscaling.HorizontalPodAutoscalerList
+	if err := r.List(ctx, &hpas, client.InNamespace(req.Namespace), client.MatchingFields{util.KubeObjectOwnerKey: req.Name}); err != nil {
+		log.Error(err, "Unable to list associated HPAs")
+		return ctrl.Result{}, err
+	}
+
+	// TODO: Fetch the VPAs associated with this strategy.
 
 	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the TemplateStrategyReconciler.
 func (r *TemplateStrategyReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	ctx := context.Background()
+	extractOwnerName := util.CreateOwnerNameExtractor(elasticityStrategies.GroupVersion, "TemplateStrategy")
+
+	// Create an index for HPAs
+	if err := mgr.GetFieldIndexer().IndexField(ctx, &autoscaling.HorizontalPodAutoscaler{}, util.KubeObjectOwnerKey, extractOwnerName); err != nil {
+		return err
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&elasticitystrategiesv1.TemplateStrategy{}).
+		For(&elasticityStrategies.TemplateStrategy{}).
+		// Owns() makes sure that a change in an HPA will also trigger a Reconcile() call.
+		Owns(&autoscaling.HorizontalPodAutoscaler{}).
 		Complete(r)
 }
