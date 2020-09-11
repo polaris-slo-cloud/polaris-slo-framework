74265c127e52aaf470a90e8be56340226be231e4
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-09-11 17:48:04 +0200

    Add HorizontalElasticityStrategy

---
+++ go/apis/elasticitystrategies/v1/horizontalelasticitystrategy_types.go
@@ -0,0 +1,61 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// HorizontalElasticityStrategySpec defines the desired state of HorizontalElasticityStrategy
+type HorizontalElasticityStrategySpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of HorizontalElasticityStrategy. Edit HorizontalElasticityStrategy_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// HorizontalElasticityStrategyStatus defines the observed state of HorizontalElasticityStrategy
+type HorizontalElasticityStrategyStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+
+// HorizontalElasticityStrategy is the Schema for the horizontalelasticitystrategies API
+type HorizontalElasticityStrategy struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   HorizontalElasticityStrategySpec   `json:"spec,omitempty"`
+	Status HorizontalElasticityStrategyStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// HorizontalElasticityStrategyList contains a list of HorizontalElasticityStrategy
+type HorizontalElasticityStrategyList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []HorizontalElasticityStrategy `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&HorizontalElasticityStrategy{}, &HorizontalElasticityStrategyList{})
+}
--- go/main.go
+++ go/main.go
@@ -72,6 +72,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "TemplateStrategy")
 		os.Exit(1)
 	}
+	if err = (&elasticitystrategiescontroller.HorizontalElasticityStrategyReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("HorizontalElasticityStrategy"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "HorizontalElasticityStrategy")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
