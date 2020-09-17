d483dec4228b953d82f4906bbe4e7dfbf257a17c
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-09-17 15:24:07 +0200

    Add SloApplication controller

--- ts/apps/cpu-usage-slo-controller/src/app/controller/slo-application.controller.ts
+++ ts/apps/cpu-usage-slo-controller/src/app/controller/slo-application.controller.ts
@@ -1,9 +1,62 @@
-import { KubeConfig } from '@kubernetes/client-node';
+import { KubeConfig, Watch } from '@kubernetes/client-node';
+import { SloControlLoop } from '../control-loop';
+import { CpuUsageSloApplication } from '../model';
+import { ServiceLevelObjective } from '../sloc-policy-language';
+
+type SloApplication = CpuUsageSloApplication;
 
 export class SloApplicationController {
 
-    run(k8sConfig: KubeConfig): void {
+    private controlLoop: SloControlLoop;
 
+    run(k8sConfig: KubeConfig): void {
+        this.controlLoop = new SloControlLoop(k8sConfig);
+        this.controlLoop.start();
+
+        const watch = new Watch(k8sConfig);
+        watch.watch(
+            '/apis/slos.sloc.github.io/v1/cpuusagesloapplications',
+            {
+                allowWatchBookmarks: false,
+            },
+            (type, apiObj) => {
+                switch (type) {
+                    case 'ADDED':
+                    case 'MODIFIED':
+                        this.onSloApplicationAddOrUpdate(apiObj);
+                        break;
+                    case 'DELETED':
+                        this.onSloApplicationDelete(apiObj);
+                        break;
+                    default:
+                        break;
+                }
+            },
+            // done callback is called if the watch terminates normally
+            (err) => {
+                console.log(err);
+                this.controlLoop.stop();
+            })
+        .then((req) => {
+            // watch returns a request object which you can use to abort the watch.
+        });
+
+    }
+
+    private onSloApplicationAddOrUpdate(sloApplication: SloApplication): void {
+        console.log('Adding/updating SloApplication', sloApplication);
+
+        const slo = this.createSloInstance(sloApplication);
+        this.controlLoop.registerSlo(sloApplication.metadata.uid, slo);
+    }
+
+    private createSloInstance(config: SloApplication): ServiceLevelObjective<any, any> {
+        return null;
+    }
+
+    private onSloApplicationDelete(sloApplication: SloApplication): void {
+        console.log('Deleting SloApplication', sloApplication);
+        this.controlLoop.unregisterSlo(sloApplication.metadata.uid);
     }
 
 }
--- ts/apps/cpu-usage-slo-controller/src/main.ts
+++ ts/apps/cpu-usage-slo-controller/src/main.ts
@@ -1,38 +1,8 @@
-import * as k8s from '@kubernetes/client-node';
+import { KubeConfig } from "@kubernetes/client-node";
+import { SloApplicationController } from './app/controller';
 
-const kc = new k8s.KubeConfig();
-kc.loadFromDefault();
+const k8sConfig = new KubeConfig();
+k8sConfig.loadFromDefault();
 
-const watch = new k8s.Watch(kc);
-watch.watch('/apis/slos.sloc.github.io/v1/cpuusagesloapplications',
-    // optional query parameters can go here.
-    {
-        allowWatchBookmarks: true,
-    },
-    // callback is called for each received object.
-    (type, apiObj) => {
-        if (type === 'ADDED') {
-            // tslint:disable-next-line:no-console
-            console.log('new object:');
-        } else if (type === 'MODIFIED') {
-            // tslint:disable-next-line:no-console
-            console.log('changed object:');
-        } else if (type === 'DELETED') {
-            // tslint:disable-next-line:no-console
-            console.log('deleted object:');
-        } else {
-            // tslint:disable-next-line:no-console
-            console.log('unknown type: ' + type);
-        }
-        // tslint:disable-next-line:no-console
-        console.log(apiObj);
-    },
-    // done callback is called if the watch terminates normally
-    (err) => {
-        // tslint:disable-next-line:no-console
-        console.log(err);
-    })
-.then((req) => {
-    // watch returns a request object which you can use to abort the watch.
-    setTimeout(() => { req.abort(); }, 10 * 1000);
-});
+const controller = new SloApplicationController();
+controller.run(k8sConfig);
