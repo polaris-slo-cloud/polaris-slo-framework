c2a2a44444f7e0e03141f020bc253c13e4ed7f40
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-09-29 15:40:30 +0200

    Add staticElasticityStrategyParams

--- ts/libs/core/src/lib/model/slo-mapping.ts
+++ ts/libs/core/src/lib/model/slo-mapping.ts
@@ -1,5 +1,5 @@
 import { SlocType } from '../transformation';
-import { initSelf } from '../util';
+import { IndexByKey, initSelf } from '../util';
 import { ElasticityStrategyKind } from './elasticity-strategy-kind';
 import { ObjectReference } from './object-reference';
 
@@ -15,6 +15,31 @@ export abstract class SloMappingBase {
     /** Specifies the type of ElasticityStrategy to use for this SLO mapping. */
     @SlocType(() => ElasticityStrategyKind)
     elasticityStrategy: ElasticityStrategyKind;
+
+    /**
+     * Any static parameters, which are unknown to the SLO, but which may be required to configure
+     * the chosen kind of elasticity strategy should be specified here. They will be copied over
+     * into the spec of the elasticity strategy.
+     *
+     * For example, suppose the SLO knows only about the parameters in `SloCompliance`, but you want
+     * to use an elasticity strategy that requires an additional parameter, e.g., `maxReplicas`.
+     * This can be configured when instantiating the SloMapping:
+     *
+     * ```
+     * new MySloMapping({
+     *      elasticityStrategy: new ElasticityStrategyKind({
+     *          kind: 'my-special-elasticity-strategy-kind',
+     *          ...
+     *      }),
+     *      ...,
+     *      staticElasticityStrategyParams: {
+     *          // Anything in here will be copied over to the spec of the elasticity strategy.
+     *          maxReplicas: 100,
+     *      },
+     * });
+     * ```
+     */
+    staticElasticityStrategyParams?: IndexByKey<any>;
 
     constructor(initData?: Partial<SloMappingBase>) {
         initSelf(this, initData);
