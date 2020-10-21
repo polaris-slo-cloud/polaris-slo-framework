e9a503bb15eb12644cd3176097635a359b83d1cc
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-10-21 13:59:12 +0200

    Expose the stopper subject, because takeUntil() requires an Observable in RxJS 6.x (his is going to change in RxJS 7.x)

--- ts/libs/core/src/lib/util/public/observable-stopper.ts
+++ ts/libs/core/src/lib/util/public/observable-stopper.ts
@@ -1,53 +1,57 @@
-/* eslint-disable @typescript-eslint/unified-signatures */
-import { PartialObserver, Subject, Subscribable, Unsubscribable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 /**
- * This is a multicast observable that can be used to control the completion of another observable.
+ * This helper can be used to control the completion of another observable.
  *
  * Example:
  * ```
- * private stopper$ = new ObservableStopper();
+ * private stopper = new ObservableStopper();
  *
  * ngOnInit(): void {
  *      this.myObservable$.pipe(
  *          ...,
- *          takeUntil(this.stopper$),
+ *          takeUntil(this.stopper.stopper$),
  *      ).subscribe((value) => {
  *          ...
  *      })
  * }
  *
  * ngOnDestroy(): void {
- *      this.stopper$.stop();
+ *      this.stopper.stop();
  * }
  * ```
  */
-export class ObservableStopper implements Subscribable<void> {
+export class ObservableStopper {
 
-    private stopper$ = new Subject<void>();
+    private stopperSubj$ = new Subject<void>();
+
+    // Used to conceal the Subject when providing the stopper publicly.
+    private pubStopper$: Observable<void>;
+
+    /**
+     * The multicast observable that will emit and complete when `stop()` is called.
+     */
+    get stopper$(): Observable<void> {
+        return this.pubStopper$;
+    }
+
+    constructor() {
+        this.pubStopper$ = this.stopperSubj$.asObservable();
+    }
 
     /**
      * @returns `true` if this `ObservableStopper` has been stopped, otherwise `false.
      */
     get isStopped(): boolean {
-        return this.stopper$.isStopped;
-    }
-
-    subscribe(observer?: PartialObserver<void>): Unsubscribable;
-    subscribe(next: null, error: null, complete: () => void): Unsubscribable;
-    subscribe(next: null, error: (error: any) => void, complete?: () => void): Unsubscribable;
-    subscribe(next: (value: void) => void, error: null, complete: () => void): Unsubscribable;
-    subscribe(next?: (value: void) => void, error?: (error: any) => void, complete?: () => void): Unsubscribable;
-    subscribe(next?: any, error?: any, complete?: any): Unsubscribable {
-        return this.stopper$.subscribe(next, error, complete);
+        return this.stopperSubj$.isStopped;
     }
 
     /**
      * Instructs all observables depending on this `ObservableStopper` to complete.
      */
     stop(): void {
-        this.stopper$.next();
-        this.stopper$.complete();
+        this.stopperSubj$.next();
+        this.stopperSubj$.complete();
     }
 
 }
