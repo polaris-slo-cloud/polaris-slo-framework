39b7de74e9763e9c7b0bb1d91c1c650f32143738
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-11-16 15:58:01 +0100

    Run nx migrations

--- ts/apps/slo/cpu-usage-slo-controller/jest.config.js
+++ ts/apps/slo/cpu-usage-slo-controller/jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    name: 'slo-cpu-usage-slo-controller',
-    preset: '../../../jest.config.js',
+    preset: '../../../jest.preset.js',
     globals: {
         'ts-jest': {
             tsConfig: '<rootDir>/tsconfig.spec.json',
-        }
+        },
     },
     transform: {
-        '^.+\\.[tj]s$': 'ts-jest'
+        '^.+\\.[tj]s$': 'ts-jest',
     },
     moduleFileExtensions: ['ts', 'js', 'html'],
-    coverageDirectory: '../../../coverage/apps/slo/cpu-usage-slo-controller'
+    coverageDirectory: '../../../coverage/apps/slo/cpu-usage-slo-controller',
+    displayName: 'slo-cpu-usage-slo-controller',
 };
