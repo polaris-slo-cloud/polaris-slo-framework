01abb82b05128e0a919250aa33fbbd0316af0bab
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-11-17 19:44:46 +0100

    Remove slo-generic library project

--- ts/libs/slo/slo-generic/src/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './lib/slo-slo-generic';
--- ts/libs/slo/slo-generic/src/lib/slo-slo-generic.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sloSloGeneric } from './slo-slo-generic';
-
-describe('sloSloGeneric', () => {
-    it('should work', () => {
-        expect(sloSloGeneric()).toEqual('slo-slo-generic');
-    });
-});
