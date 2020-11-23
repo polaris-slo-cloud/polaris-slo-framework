024c865c6bf723015f0f43a5873324935221c2c2
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-11-23 17:45:43 +0100

    Fix configuration type

--- ts/apps/slo/cpu-usage-slo-controller/src/app/cpu-usage-slo/cpu-usage.slo.ts
+++ ts/apps/slo/cpu-usage-slo-controller/src/app/cpu-usage-slo/cpu-usage.slo.ts
@@ -1,17 +1,17 @@
-import { CostEfficiencySloConfig } from '@sloc/common-mappings';
+import { CpuUsageSloConfig } from '@sloc/common-mappings';
 import { MetricsSource, ObservableOrPromise, ServiceLevelObjective, SloCompliance, SloMappingSpec, SloOutput, SlocRuntime } from '@sloc/core';
 import { of as observableOf } from 'rxjs';
 
 const LOWER_BOUND = 1;
 const UPPER_BOUND = 200;
 
-export class CpuUsageSlo implements ServiceLevelObjective<CostEfficiencySloConfig, SloCompliance>  {
+export class CpuUsageSlo implements ServiceLevelObjective<CpuUsageSloConfig, SloCompliance>  {
 
-    spec: SloMappingSpec<CostEfficiencySloConfig, SloCompliance>;
+    spec: SloMappingSpec<CpuUsageSloConfig, SloCompliance>;
 
     private metricsSource: MetricsSource;
 
-    configure(spec: SloMappingSpec<CostEfficiencySloConfig, SloCompliance>, metricsSource: MetricsSource, slocRuntime: SlocRuntime): ObservableOrPromise<void> {
+    configure(spec: SloMappingSpec<CpuUsageSloConfig, SloCompliance>, metricsSource: MetricsSource, slocRuntime: SlocRuntime): ObservableOrPromise<void> {
         this.spec = spec;
         this.metricsSource = metricsSource;
         return observableOf(null);
