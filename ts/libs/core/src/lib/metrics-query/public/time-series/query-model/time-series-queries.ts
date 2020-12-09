3cb4a110c40a4460ca187f7b4399673f1493da7d
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-12-09 18:16:09 +0100

    Support value filtering also on TimeRangeQuery

--- ts/libs/core/src/lib/metrics-query/public/time-series/query-model/time-series-queries.ts
+++ ts/libs/core/src/lib/metrics-query/public/time-series/query-model/time-series-queries.ts
@@ -11,9 +11,9 @@ import { ValueFilter } from './value-filter';
  * - `TimeRangeQuery`, whose execution results in an array of `TimeSeries`, each normally containing multiple samples.
  * - `TimeInstantQuery`, whose execution results in an array of `TimeSeriesInstant` objects, each containing a single sample.
  *
- * `TimeInstantQuery` also allows applying filters on the values of the `TimeSeries`.
+ * Both main query types allow applying filters on the values of the `TimeSeries`.
  *
- * Both main query types may be combined with `LabelFilterableQuery`, which allows filtering on labels.
+ * The main query types may be combined with `LabelFilterableQuery`, which allows filtering on labels.
  * A query resulting from a `TimeSeriesSource.select()` will be either
  * - a `LabelFilterableTimeRangeQuery = TimeRangeQuery & LabelFilterableQuery`, if a `TimeRange` was specified, or
  * - a `LabelFilterableTimeInstantQueryQuery = TimeInstantQuery & LabelFilterableQuery`, if no `TimeRange` was specified.
@@ -105,7 +105,7 @@ export interface LabelFilterableQuery<T extends TimeSeries<any>, Q extends Label
  *
  * @note Some methods may return a query of a different type, e.g., a `TimeInstantQuery`.
  */
-export interface TimeRangeQuery<T> extends TimeSeriesQuery<TimeSeries<T>> {
+export interface TimeRangeQuery<T> extends ValueFilterableQuery<TimeSeries<T>, TimeRangeQuery<T>> {
 
     /**
      * Counts the number of times the value changes for each `TimeSeries` and returns that
