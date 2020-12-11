81df0530620f6f70e1690e19ffc1fb12ca8a1d3e
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-12-11 20:41:58 +0100

    Copy and adapt CPU usage SLO files for cost efficiency SLO

--- ts/apps/slo/cost-efficiency-slo-controller/src/main.ts
+++ ts/apps/slo/cost-efficiency-slo-controller/src/main.ts
@@ -1,1 +1,32 @@
-console.log('Hello World!');
+import { KubeConfig } from '@kubernetes/client-node';
+import { CostEfficiencySloMapping, CostEfficiencySloMappingSpec, initSlocLib as initCommonMappingsLib } from '@sloc/common-mappings';
+import { initSlocKubernetes } from '@sloc/kubernetes';
+import { interval } from 'rxjs';
+import { CostEfficiencySlo } from './app/cost-efficiency-slo';
+
+// ToDo: This file should be generated automatically during the build process.
+// ToDo: It should be possible to build the SLO controller easily for multiple orchestrators.
+
+// Load the KubeConfig and initialize the @sloc/kubernetes library.
+const k8sConfig = new KubeConfig();
+k8sConfig.loadFromDefault();
+const slocRuntime = initSlocKubernetes(k8sConfig);
+
+// Initialize the used SLOC mapping libraries
+initCommonMappingsLib(slocRuntime);
+
+// Create an SloControlLoop and register the factories for the ServiceLevelObjectives it will handle
+const sloControlLoop = slocRuntime.createSloControlLoop();
+sloControlLoop.microcontrollerFactory.registerFactoryFn(CostEfficiencySloMappingSpec, () => new CostEfficiencySlo());
+
+// Create an SloEvaluator and start the control loop with an interval of 20 seconds.
+const sloEvaluator = slocRuntime.createSloEvaluator();
+sloControlLoop.start({
+    evaluator: sloEvaluator,
+    interval$: interval(20000),
+});
+
+// Create a WatchManager and watch the supported SLO mapping kinds.
+const watchManager = slocRuntime.createWatchManager();
+watchManager.startWatchers([ new CostEfficiencySloMapping().objectKind ], sloControlLoop.watchHandler)
+    .catch(error => void console.error(error))
