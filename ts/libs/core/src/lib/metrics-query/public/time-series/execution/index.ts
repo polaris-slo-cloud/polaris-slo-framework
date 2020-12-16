d01ed9c5fa7f4a7824f06c3cb9ec7ec36c56ec36
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-12-16 18:23:30 +0100

    Add TimeSeriesSourceBase

--- ts/libs/core/src/lib/metrics-query/public/time-series/execution/index.ts
+++ ts/libs/core/src/lib/metrics-query/public/time-series/execution/index.ts
@@ -3,3 +3,4 @@ export * from './native-query-builder';
 export * from './query-content';
 export * from './time-series-queries.impl';
 export * from './time-series-query.base';
+export * from './time-series-source.base';
