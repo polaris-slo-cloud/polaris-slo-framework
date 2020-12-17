9315f58abbad4e5fa54979df43f71c8b39ea33bf
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2020-12-17 16:23:27 +0100

    Implement LabelFilters

--- ts/libs/query-backends/prometheus/src/lib/time-series/internal/prometheus-native-query-builder.ts
+++ ts/libs/query-backends/prometheus/src/lib/time-series/internal/prometheus-native-query-builder.ts
@@ -1,4 +1,12 @@
-import { NativeQueryBuilderBase, TimeSeriesQuery, TimeSeriesQueryResultType } from '@sloc/core';
+import {
+    FilterOnLabelQueryContent,
+    LabelComparisonOperator,
+    LabelFilter,
+    NativeQueryBuilderBase,
+    QueryContentType,
+    TimeSeriesQuery,
+    TimeSeriesQueryResultType,
+} from '@sloc/core';
 import { PrometheusConfig } from '../../config';
 import { PrometheusNativeQuery } from './prometheus-native-query';
 
@@ -14,11 +22,45 @@ export class PrometheusNativeQueryBuilder extends NativeQueryBuilderBase {
     }
 
     private buildPromQlQuery(): string {
-        const query = `${this.selectSegment.appName}_${this.selectSegment.metricName}`;
+        const selectStatement = `${this.selectSegment.appName}_${this.selectSegment.metricName}`;
+        const labelFilters: string[] = [];
 
-        // ToDo
+        this.queryChainAfterSelect.forEach(segment => {
+            switch (segment.contentType) {
+                case QueryContentType.FilterOnLabel:
+                    labelFilters.push(this.buildLabelFilter((segment as FilterOnLabelQueryContent).filter));
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        let query = selectStatement;
+        if (labelFilters.length > 0) {
+            const filterStr = labelFilters.join(',');
+            query = `${selectStatement}{${filterStr}}`;
+        }
 
         return query;
+    }
+
+    private buildLabelFilter(labelFilter: LabelFilter): string {
+        let operator: string;
+        switch (labelFilter.operator) {
+            case LabelComparisonOperator.Equal:
+                operator = '=';
+                break;
+            case LabelComparisonOperator.NotEqual:
+                operator = '!=';
+                break;
+            case LabelComparisonOperator.RegexMatch:
+                operator = '=~';
+                break;
+            default:
+                break;
+        }
+
+        return `${labelFilter.label}${operator}'${labelFilter.comparisonValue}'`;
     }
 
 }
