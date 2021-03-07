b01296ae170f9ce43c2e9527448ae1405b96de3f
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2021-03-07 19:25:28 +0100

    Define PolishedMetricSource and factory

--- ts/libs/core/src/lib/polished-metrics/public/common/polished-metric-source-factory.ts
+++ ts/libs/core/src/lib/polished-metrics/public/common/polished-metric-source-factory.ts
@@ -1,8 +1,23 @@
+import { SloTarget } from '../../../model';
+import { PolishedMetricSource } from './polished-metric-source';
 
 /**
- * A `PolishedMetricFactory` is used to create a `PolishedMetric` instance that is scoped
+ * A `PolishedMetricSourceFactory` is used to create a `PolishedMetricSource` instance that is scoped
  * to a particular `SloTarget`.
+ *
+ * @param V The TypeScript type that represents the values of the polished metric.
+ * @param T The type of `SloTarget` that the polished metric can be obtained from.
+ * @param P Optional parameters that can be used to configure the `PolishedMetricSource`.
  */
-export interface PolishedMetricFactory {
+export interface PolishedMetricSourceFactory<V, T extends SloTarget = SloTarget, P = never> {
+
+    /**
+     * Creates a new `PolishedMetricSource` for the specified `sloTarget` and the optional `params`.
+     *
+     * @param sloTarget The target workload, where the metric should be obtained from.
+     * @param params (optional) Parameters to further configure the metric source.
+     * @returns A new `PolishedMetricSource`.
+     */
+    createSource(sloTarget: T, params?: P): PolishedMetricSource<V>;
 
 }
--- ts/libs/core/src/lib/polished-metrics/public/common/polished-metric-source.ts
+++ ts/libs/core/src/lib/polished-metrics/public/common/polished-metric-source.ts
@@ -1,8 +1,21 @@
+import { Observable } from 'rxjs';
 
 /**
- * A `PolishedMetricSource` is used to obtain a complex, processed metric from a deployment.
+ * A `PolishedMetricSource` is used to obtain a complex, processed metric from an `SloTarget`.
+ *
+ * @param V The TypeScript type that represents the values of the polished metric.
  */
-export interface PolishedMetricSource {
-    // ToDo
-    // We need parameters to specify the deployment
+export interface PolishedMetricSource<V> {
+
+    /**
+     * @returns An observable that emits the current value of the polished metric and then completes.
+     */
+    getCurrentValue(): Observable<V>;
+
+    /**
+     * @returns An observable that emits the polished metric's values as they become available, starting with
+     * the current value.
+     */
+    getValueStream(): Observable<V>;
+
 }
