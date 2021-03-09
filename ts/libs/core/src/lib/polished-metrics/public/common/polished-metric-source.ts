674f52299a0e6c5fef75363a3f532a5089da2512
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2021-03-09 14:50:02 +0100

    Wrap polished metrics in a Sample to add a timestamp

--- ts/libs/core/src/lib/polished-metrics/public/common/polished-metric-source.ts
+++ ts/libs/core/src/lib/polished-metrics/public/common/polished-metric-source.ts
@@ -1,4 +1,5 @@
 import { Observable } from 'rxjs';
+import { Sample } from '../../../raw-metrics-query/public';
 
 /**
  * A `PolishedMetricSource` is used to obtain a complex, processed metric from an `SloTarget`.
@@ -10,12 +11,12 @@ export interface PolishedMetricSource<V> {
     /**
      * @returns An observable that emits the current value of the polished metric and then completes.
      */
-    getCurrentValue(): Observable<V>;
+    getCurrentValue(): Observable<Sample<V>>;
 
     /**
      * @returns An observable that emits the polished metric's values as they become available, starting with
      * the current value.
      */
-    getValueStream(): Observable<V>;
+    getValueStream(): Observable<Sample<V>>;
 
 }
