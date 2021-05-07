c2367d384dddb47f21298351ba73b437906d6050
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2021-05-07 12:06:41 +0200

    Add installation of polaris core package

--- ts/libs/polaris-nx/src/generators/slo-mapping-type/generator.ts
+++ ts/libs/polaris-nx/src/generators/slo-mapping-type/generator.ts
@@ -1,10 +1,11 @@
 import {
     Generator,
     Tree,
+    addDependenciesToPackageJson,
     formatFiles,
     joinPathFragments,
 } from '@nrwl/devkit';
-import { addExportToIndex } from '../../util';
+import { NPM_PACKAGES, VERSIONS, addExportToIndex } from '../../util';
 import { addFiles } from './lib/add-files';
 import { normalizeOptions } from './lib/normalize-options';
 import { SloMappingTypeGeneratorSchema } from './schema';
@@ -15,6 +16,18 @@ import { SloMappingTypeGeneratorSchema } from './schema';
 const generatorFn: Generator<SloMappingTypeGeneratorSchema> = async (host: Tree, options: SloMappingTypeGeneratorSchema) => {
     const normalizedOptions = normalizeOptions(host, options);
 
+    // Add required packages to package.json, if necessary.
+    const installPkgsFn = addDependenciesToPackageJson(
+        host,
+        {
+            [NPM_PACKAGES.polaris.core]: VERSIONS.polaris,
+        },
+        {},
+    );
+
+    // Adapt tsconfig to allow decorators.
+
+    // Generate the SLO mapping file.
     addFiles(host, normalizedOptions);
 
     // Export the contents of the new SLO mapping file from the library.
@@ -23,6 +36,8 @@ const generatorFn: Generator<SloMappingTypeGeneratorSchema> = async (host: Tree,
     addExportToIndex(host, indexFile, sloMappingFile);
 
     await formatFiles(host);
+
+    return installPkgsFn;
 }
 
 export default generatorFn;
