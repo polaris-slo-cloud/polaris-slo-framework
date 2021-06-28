88292e21aaa4eb6dc6815fafd99af7d717f70044
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2021-06-28 19:34:02 +0200

    Refactor HorizontalElasticityStrategyControllerBase into a reusable superclass

--- ts/libs/core/src/lib/elasticity/public/control/base/horizontal-elasticity-strategy-controller.base.ts
+++ ts/libs/core/src/lib/elasticity/public/control/base/horizontal-elasticity-strategy-controller.base.ts
@@ -1,4 +1,4 @@
-import { ElasticityStrategy, NamespacedObjectReference, SloCompliance, SloTarget } from '../../../../model';
+import { ElasticityStrategy, NamespacedObjectReference, Scale, SloCompliance, SloTarget } from '../../../../model';
 import { OrchestratorClient, PolarisRuntime } from '../../../../runtime';
 import { HorizontalElasticityStrategyConfig } from '../../common';
 import { SloComplianceElasticityStrategyControllerBase } from './slo-compliance-elasticity-strategy-controller.base';
@@ -6,48 +6,78 @@ import { SloComplianceElasticityStrategyControllerBase } from './slo-compliance-
 /**
  * Common superclass for controller for executing an elasticity strategy that employs horizontal scaling.
  */
-export abstract class HorizontalElasticityStrategyControllerBase
-    extends SloComplianceElasticityStrategyControllerBase<SloTarget, HorizontalElasticityStrategyConfig> {
+export abstract class HorizontalElasticityStrategyControllerBase<T extends SloTarget, C extends HorizontalElasticityStrategyConfig>
+    extends SloComplianceElasticityStrategyControllerBase<T, C> {
 
-    private orchClient: OrchestratorClient;
+    /** The client for accessing orchestrator resources. */
+    protected orchClient: OrchestratorClient;
 
-    constructor(private polarisRuntime: PolarisRuntime) {
+    /** The `PolarisRuntime` instance. */
+    protected polarisRuntime: PolarisRuntime
+
+    constructor(polarisRuntime: PolarisRuntime) {
         super();
+        this.polarisRuntime = polarisRuntime;
         this.orchClient = polarisRuntime.createOrchestratorClient();
     }
 
-    async execute(elasticityStrategy: ElasticityStrategy<SloCompliance, SloTarget, HorizontalElasticityStrategyConfig>): Promise<void> {
+    /**
+     * Computes the new number of replicas according to the the `elasticityStrategy` instance.
+     *
+     * The resulting new replica count will be normalized using the min/max allowed replicas by the
+     * caller of this method.
+     *
+     * @param elasticityStrategy The current elasticity strategy instance.
+     * @param currScale The current state of the target workload's `Scale` subresource.
+     * @returns A promise that resolves to a `Scale` object that contains the new number of replicas.
+     */
+    protected abstract computeScale(elasticityStrategy: ElasticityStrategy<SloCompliance, T, C>, currScale: Scale): Promise<Scale>;
+
+    async execute(elasticityStrategy: ElasticityStrategy<SloCompliance, T, C>): Promise<void> {
         console.log('Executing elasticity strategy:', elasticityStrategy);
 
         const targetRef = new NamespacedObjectReference({
             namespace: elasticityStrategy.metadata.namespace,
             ...elasticityStrategy.spec.targetRef,
         });
-        const scale = await this.orchClient.getScale(targetRef);
+        const currScale = await this.orchClient.getScale(targetRef);
+        const oldReplicaCount = currScale.spec.replicas;
 
-        const multiplier = elasticityStrategy.spec.sloOutputParams.currSloCompliancePercentage / 100;
-        let newReplicaCount = Math.ceil(scale.spec.replicas * multiplier);
-        newReplicaCount = Math.max(newReplicaCount, this.getMinReplicas(elasticityStrategy.spec.staticConfig));
-        newReplicaCount = Math.min(newReplicaCount, this.getMaxReplicas(elasticityStrategy.spec.staticConfig));
+        const newScale = await this.computeScale(elasticityStrategy, currScale);
+        newScale.spec.replicas = this.normalizeReplicaCount(newScale.spec.replicas, elasticityStrategy.spec.staticConfig);
 
-        if (scale.spec.replicas === newReplicaCount) {
+        if (newScale.spec.replicas === oldReplicaCount) {
             console.log(
                 'No scaling possible, because new replica count after min/max check is equal to old replica count.',
-                scale,
+                newScale,
             );
         }
 
-        scale.spec.replicas = newReplicaCount;
-        await this.orchClient.setScale(targetRef, scale);
-        console.log('Successfully updated scale subresource:', scale);
+        await this.orchClient.setScale(targetRef, newScale);
+        console.log('Successfully updated scale subresource:', newScale);
     }
 
-    private getMinReplicas(config: HorizontalElasticityStrategyConfig): number {
+    /**
+     * @returns `config?.minReplicas`, if it is set, or, otherwise, the default min replicas value.
+     */
+    protected getMinReplicas(config: C): number {
         return config?.minReplicas ?? 1;
     }
 
-    private getMaxReplicas(config: HorizontalElasticityStrategyConfig): number {
+    /**
+     * @returns `config?.maxReplicas`, if it is set, or, otherwise, the default max replicas value.
+     */
+    protected getMaxReplicas(config: C): number {
         return config?.maxReplicas ?? Infinity;
+    }
+
+    /**
+     * Normalizes the `newReplicaCount` to be within the min/max range from the `config`.
+     */
+    private normalizeReplicaCount(newReplicaCount: number, config: C): number {
+        newReplicaCount = Math.max(newReplicaCount, this.getMinReplicas(config));
+        newReplicaCount = Math.min(newReplicaCount, this.getMaxReplicas(config));
+        return newReplicaCount;
     }
 
 }
