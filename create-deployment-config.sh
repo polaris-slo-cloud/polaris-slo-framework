72257c3ff24845ae6fc3e7c3b877d8754a0addcc
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2021-07-18 19:16:50 +0200

    Create deployment YAML using TS elasticity strategy controllers

---
+++ create-deployment-config.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -x
+set -o errexit
+
+# ToDo: Add possibility to specify version, because currently the version must be manually set in th econtrollers' YAML files.
+
+OUTPUT="deployment/1-elasticity-strategies.yaml"
+
+# Delete old config.
+rm -rf "$OUTPUT"
+
+
+# Build the projects.
+INPUT_YAML_FILES=(
+    # CRDs
+    "go/config/crd/bases/elasticity.polaris-slo-cloud.github.io_horizontalelasticitystrategies.yaml"
+    "go/config/crd/bases/elasticity.polaris-slo-cloud.github.io_verticalelasticitystrategies.yaml"
+    "go/config/crd/bases/slo.polaris-slo-cloud.github.io_costefficiencyslomappings.yaml"
+    "go/config/crd/bases/slo.polaris-slo-cloud.github.io_cpuusageslomappings.yaml"
+
+    # Horizontal Elasticity Strategy controller
+    "ts/apps/elasticity/horizontal-elasticity-strategy-controller/manifests/1-rbac.yaml"
+    "ts/apps/elasticity/horizontal-elasticity-strategy-controller/manifests/2-elasticity-strategy-controller.yaml"
+
+    # Vertical Elasticity Strategy controller
+    "ts/apps/elasticity/vertical-elasticity-strategy-controller/manifests/1-rbac.yaml"
+    "ts/apps/elasticity/vertical-elasticity-strategy-controller/manifests/2-elasticity-strategy-controller.yaml"
+)
+
+for inputPath in ${INPUT_YAML_FILES[@]}; do
+    cat "$inputPath" >> "$OUTPUT"
+    echo -e "\n---\n" >> "$OUTPUT"
+done
+
+echo "Successfully wrote deployment config to $OUTPUT"
