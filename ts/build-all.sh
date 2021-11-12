56aaa14d0d85526d5eed64ab4167531164247ef5
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2021-11-12 18:12:24 +0100

    Add Pull request template with checklist

---
+++ ts/build-all.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# set -x
+set -o errexit
+
+# Delete old builds.
+rm -rf ./dist
+rm -rf ./tmp
+
+
+# Build the projects.
+PROJECTS=(
+    "core"
+    "mappings-common-mappings"
+    "orchestrators-kubernetes"
+    "query-backends-prometheus"
+    "metrics-cost-efficiency"
+    "schema-gen"
+    "polaris-nx"
+    "cli-polaris-cli"
+    "cli-polaris-k8s-serializer"
+    "elasticity-horizontal-elasticity-strategy-controller"
+    "elasticity-vertical-elasticity-strategy-controller"
+    "metrics-rest-api-cost-efficiency-controller"
+    "slo-cost-efficiency-slo-controller"
+    "slo-cpu-usage-slo-controller"
+    "ui-polaris-ui"
+)
+
+for proj in ${PROJECTS[@]}; do
+    npx nx build $proj
+done
+
+
+echo "All packages were successfully built."
