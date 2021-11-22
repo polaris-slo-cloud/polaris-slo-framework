f09a8a3332b601f46421067434fd97f75f121a40
Philipp <poepsi39@gmail.com>
2021-11-22 16:30:37 +0100

    Add basic prometheus client

---
+++ python/controllers/predicted/query/prometheus.py
@@ -0,0 +1,14 @@
+import json
+from typing import Dict
+
+import requests
+
+
+class PrometheusClient:
+
+    def __init__(self, base_url: str):
+        self.base_url = base_url
+
+    def query(self, query: str) -> Dict:
+        url = f'http://{self.base_url}/api/v1/query?query={query}'
+        return requests.get(url).json()
