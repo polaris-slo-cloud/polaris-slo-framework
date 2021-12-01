33e42d852dfebdd1ff61b7976bcc2aa1ebe23376
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2021-12-01 12:33:36 +0100

    Bump to v0.2.0-beta.20

--- ts/libs/polaris-nx/src/util/packages.ts
+++ ts/libs/polaris-nx/src/util/packages.ts
@@ -7,7 +7,7 @@ export const POLARIS_NPM_ORG = '@polaris-sloc';
  * Defines the versions of packages added by the generators.
  */
 export const VERSIONS = {
-    polaris: '0.2.0-beta.19',
+    polaris: '0.2.0-beta.20',
     rxJs: '^6.6.7',
     tsNode: '^10.3.0',
     tsNodeConfigPaths: '^3.11.0',
