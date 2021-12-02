3d96ec8fcbb3dfe76d0870d6e8219fec70604ee4
Philipp Raith <p.raith@dsg.tuwien.ac.at>
2021-12-02 17:06:27 +0100

    Integrate predicted metrics controller

--- ts/apps/metrics/predicted-cost-efficiency-controller/src/app/metrics/cost-efficiency/cost-efficiency.metric-source.ts
+++ ts/apps/metrics/predicted-cost-efficiency-controller/src/app/metrics/cost-efficiency/cost-efficiency.metric-source.ts
@@ -2,9 +2,10 @@
 import { CostEfficiency, CostEfficiencyParams } from '@polaris-sloc/common-mappings';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { ComposedMetricSourceBase, MetricsSource, OrchestratorGateway, Sample } from '@polaris-sloc/core';
-import Axios from 'axios-observable';
+import axios from 'axios';
 import { Observable } from 'rxjs';
-import { map, withLatestFrom } from 'rxjs/operators';
+import { map, mergeMap, switchMap, withLatestFrom } from 'rxjs/operators';
+import { getEnvironmentVariable } from '../../util/environment-var-helper';
 // ToDo:
 // 1. Adapt the list of `supportedSloTargetTypes` in `CostEfficiencyMetricSourceFactory` (see cost-efficiency.metric-source.factory.ts).
 // 2. Adapt the `CostEfficiencyMetricSourceFactory.metricSourceName`, if needed (e.g., if there are multiple sources for CostEfficiencyMetric that differ
@@ -16,22 +17,45 @@ import { map, withLatestFrom } from 'rxjs/operators';
  * Computes the `CostEfficiency` composed metric.
  */
 export class CostEfficiencyMetricSource extends ComposedMetricSourceBase<CostEfficiency> {
+    private readonly baseUrl: string;
+
     constructor(private params: CostEfficiencyParams, metricsSource: MetricsSource, orchestrator: OrchestratorGateway) {
         super(metricsSource, orchestrator);
+        this.baseUrl = getEnvironmentVariable('PREDICTION_SERVICE_BASE_URL');
+        if (this.baseUrl == null) {
+            this.baseUrl = 'predicted-metrics-container:5000';
+        }
+        console.log(`Using ${this.baseUrl} as baseUrl.`);
     }
 
     getValueStream(): Observable<Sample<CostEfficiency>> {
-        const a = {
-            timestamp: Math.floor(Date.now() / 1000),
-            value: {
-                costEfficiency: 1,
-                percentileBetterThanThreshold: -1,
-                totalCost: { currentCostPerHour: -1, accumulatedCostInPeriod: -1 },
-            },
-        };
         return this.getDefaultPollingInterval().pipe(
-            withLatestFrom(Axios.get('https://jsonplaceholder.typicode.com/todos/1')),
-            map(b => a),
+            switchMap(() => call(this.baseUrl)),
+            map(mapResponseToSample),
         );
     }
+}
+
+interface PredictionApiResponse {
+    predictions: number[];
+}
+
+async function call(baseUrl: string): Promise<PredictionApiResponse> {
+    const response = await axios.get<PredictionApiResponse>(baseUrl);
+    console.log(response.data);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    return Promise.resolve(response.data);
+}
+
+function mapResponseToSample(response: PredictionApiResponse): Sample<CostEfficiency> {
+    console.log(response);
+    const a = {
+        timestamp: Math.floor(Date.now() / 1000),
+        value: {
+            costEfficiency: response.predictions[0],
+            percentileBetterThanThreshold: -1,
+            totalCost: { currentCostPerHour: -1, accumulatedCostInPeriod: -1 },
+        },
+    };
+    return a;
 }
