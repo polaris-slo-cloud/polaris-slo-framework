1d0214789cc4a1f8e3107685e620e6d605c64e16
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2022-03-11 18:42:21 +0100

    Upgrade dependencies

--- ts/libs/polaris-nx/src/util/packages.ts
+++ ts/libs/polaris-nx/src/util/packages.ts
@@ -9,8 +9,8 @@ export const POLARIS_NPM_ORG = '@polaris-sloc';
 export const VERSIONS = {
     polaris: '0.3.0',
     rxJs: '^6.6.7',
-    tsNode: '^10.4.0',
-    tsNodeConfigPaths: '^3.12.0',
+    tsNode: '^10.7.0',
+    tsNodeConfigPaths: '^3.13.0',
 };
 
 /** Full names of packages added by the generators. */
