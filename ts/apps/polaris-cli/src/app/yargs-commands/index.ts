f96d8b8ce4438288e852cfb577da7f76cdd148c4
mayrmartin <22145802+mayrmartin@users.noreply.github.com>
2022-10-05 19:37:38 +0000

    polaris-cli / polaris-nx: add config command

--- ts/apps/polaris-cli/src/app/yargs-commands/index.ts
+++ ts/apps/polaris-cli/src/app/yargs-commands/index.ts
@@ -1,8 +1,10 @@
 export * from './build-command';
 export * from './command';
+export * from './config-command';
 export * from './deploy-command';
 export * from './docker-build-command';
 export * from './generate-command';
 export * from './generate-crds-command';
 export * from './init-command';
 export * from './serialize-slo-mapping-command';
+
