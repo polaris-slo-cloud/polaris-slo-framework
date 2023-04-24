e5a90a0d34ccdb1a7a6d64dd764053b6241b568e
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2023-04-24 16:23:01 +0200

    Migrate other lib projects to Nx project.json

--- ts/libs/schema-gen/jest.config.ts
+++ ts/libs/schema-gen/jest.config.ts
@@ -6,15 +6,10 @@
 export default {
     displayName: 'schema-gen',
 
-    globals: {
-        'ts-jest': {
-            tsconfig: '<rootDir>/tsconfig.spec.json',
-        },
+    transform: {
+        '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
     },
     testEnvironment: 'node',
-    transform: {
-        '^.+\\.[tj]sx?$': 'ts-jest',
-    },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     coverageDirectory: '../../coverage/libs/schema-gen',
     preset: '../../jest.preset.js',
