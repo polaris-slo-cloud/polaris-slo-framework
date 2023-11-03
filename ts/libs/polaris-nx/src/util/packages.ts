d74ff723211149dc5f10d406bc4845833be3e1a7
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2023-11-03 13:20:02 +0100

    Bump to v0.7.0

--- ts/libs/polaris-nx/src/util/packages.ts
+++ ts/libs/polaris-nx/src/util/packages.ts
@@ -7,7 +7,7 @@ export const POLARIS_NPM_ORG = '@polaris-sloc';
  * Defines the versions of packages added by the generators.
  */
 export const VERSIONS = {
-    polaris: '0.6.3',
+    polaris: '0.7.0',
     rxJs: '^6.6.7',
     tsNode: '^10.9.1',
     tsNodeConfigPaths: '^4.2.0',
